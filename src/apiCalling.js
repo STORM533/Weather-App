8b26786910b4445954a3a75668070a58d4d7c878
STORM533 <146557098+STORM533@users.noreply.github.com>
2025-12-15 01:07:28 +0530

    api calling successfull

---
+++ src/apiCalling.js
@@ -0,0 +1,6 @@
+async function getWeather(input) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input}?unitGroup=metric&key=5AN4VQVYNPK8GATDWNPVPE4ZC&contentType=json`);
+    const weatherData = await response.json();
+    console.log(weatherData);
+}
+export {getWeather};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles/styles.css"
+import {getWeather} from "./apiCalling.js";
+
+function disp() {
+    const search  = document.querySelector("#searchbar");
+    const form = document.querySelector("#myForm");
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        if(search.value==='') {
+            alert('Please Enter A City');
+        }else {
+            getWeather(search.value);
+        }
+    });
+}
+disp();
